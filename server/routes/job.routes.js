7277e8d0dd03eb4e85bc81dcf782f0df20a96e93
GR4V1TY0123 <mandarekal2@gmail.com>
2025-03-05 00:25:16 +0530

    ok

--- server/routes/job.routes.js
+++ server/routes/job.routes.js
@@ -5,8 +5,8 @@ import isAuthenticated from "../middleware/isAuthenticated.js"
 const router = express.Router()
 
 router.route("/addJob").post(isAuthenticated,postJob)
-router.route("/get").get(isAuthenticated,getAllJobs)
-router.route("/get/:id").get(isAuthenticated,getJobById)
+router.route("/get").get(getAllJobs)
+router.route("/get/:id").get(getJobById)
 router.route("/getAdminJobs").get(isAuthenticated,getAdminJobs)
 
 export default router
